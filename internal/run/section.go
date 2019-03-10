22d10ac200b35f552ba05f16a22069606efd6162
Patrick <patrick.d.stephen@gmail.com>
2019-03-10 12:00:04 -0500

    Sections use sprites and generators

--- internal/run/section.go
+++ internal/run/section.go
@@ -18,16 +18,47 @@ var (
 // Todo generate section
 
 type Section struct {
-	// Todo: these should be at the generated level,
-	// made into a render.Composite, then converted into
-	// a sprite via composite.ToSprite
-	ground   [64][24]render.Renderable
-	wall     [64][12]render.Renderable
+	ground   *render.Sprite
+	wall     *render.Sprite
 	entities []characters.Character
 }
 
-func TestSection() *Section {
+type sectionGenerator struct {
+	// Todo: these should be at the generated level,
+	// made into a render.Composite, then converted into
+	// a sprite via composite.ToSprite
+	ground   [64][24]render.Modifiable
+	wall     [64][12]render.Modifiable
+	entities []characters.Character
+}
+
+func (sg *sectionGenerator) generate() *Section {
+	groundOffset := float64(oak.ScreenHeight) * 1 / 3
+	// Place ground and wall appropariately in composites and
+	// create sprites
 	s := &Section{}
+	gcmp := render.NewCompositeM()
+	for x, col := range sg.ground {
+		for y, r := range col {
+			r.SetPos(float64(x)*16, groundOffset+float64(y)*16)
+			gcmp.Append(r)
+		}
+	}
+	s.ground = gcmp.ToSprite()
+	wcmp := render.NewCompositeM()
+	for x, col := range sg.wall {
+		for y, r := range col {
+			r.SetPos(float64(x)*16, float64(y)*16)
+			wcmp.Append(r)
+		}
+	}
+	s.wall = wcmp.ToSprite()
+	s.entities = sg.entities
+	return s
+}
+
+func TestSection() *Section {
+	s := &sectionGenerator{}
 	for x := 0; x < len(s.ground); x++ {
 		for y := 0; y < len(s.ground[x]); y++ {
 			s.ground[x][y] = render.NewColorBox(
@@ -42,43 +73,24 @@ func TestSection() *Section {
 			)
 		}
 	}
+
 	ch := characters.NewChest(0)
 	ch.SetPos(800, 400)
 	s.entities = append(s.entities, ch)
-	return s
+	return s.generate()
 }
 
 func (s *Section) Draw() {
-	groundOffset := float64(oak.ScreenHeight) * 1 / 3
-	for x := 0; x < len(s.ground); x++ {
-		for y := 0; y < len(s.ground[x]); y++ {
-			s.ground[x][y].SetPos(float64(x)*16, groundOffset+float64(y)*16)
-			render.Draw(s.ground[x][y], 0)
-		}
-	}
-	for x := 0; x < len(s.wall); x++ {
-		for y := 0; y < len(s.wall[x]); y++ {
-			s.wall[x][y].SetPos(float64(x)*16, float64(y)*16)
-			render.Draw(s.wall[x][y], 0)
-		}
-	}
+	render.Draw(s.ground)
+	render.Draw(s.wall)
 	for _, e := range s.entities {
 		render.Draw(e.GetRenderable(), 2, 1)
 	}
-	// Worry about entities later
 }
 
 func (s *Section) Shift(shift float64) {
-	for x := 0; x < len(s.ground); x++ {
-		for y := 0; y < len(s.ground[x]); y++ {
-			s.ground[x][y].ShiftX(shift)
-		}
-	}
-	for x := 0; x < len(s.wall); x++ {
-		for y := 0; y < len(s.wall[x]); y++ {
-			s.wall[x][y].ShiftX(shift)
-		}
-	}
+	s.ground.ShiftX(shift)
+	s.wall.ShiftX(shift)
 	for _, e := range s.entities {
 		ShiftMoverX(e, shift)
 		fmt.Println(e.Vec().X(), e.Vec().Y())
