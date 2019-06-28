f93d35d8ce8fc01b5359afddf22beb75372e7af7
Patrick Stephen <patrick.d.stephen@gmail.com>
2019-06-28 07:17:47 -0500

    Create different chests depending on input value

--- internal/characters/doodads/chest.go
+++ internal/characters/doodads/chest.go
@@ -1,13 +1,16 @@
 package doodads
 
 import (
+	"image/color"
 	"path/filepath"
 
 	"github.com/oakmound/oak/entities"
 	"github.com/oakmound/oak/event"
 	"github.com/oakmound/oak/render"
+	"github.com/oakmound/oak/render/mod"
 
 	"github.com/oakmound/weekly87/internal/characters/labels"
+	"github.com/oakmound/weekly87/internal/recolor"
 	"github.com/oakmound/weekly87/internal/restrictor"
 )
 
@@ -40,8 +43,28 @@ func NewChest(value int64) *Chest {
 	ch := &Chest{}
 	// r := render.NewColorBox(16, 16, color.RGBA{0, 255, 255, 255})
 	r, _ := render.LoadSprite("", filepath.Join("", "/16x16/chest.png"))
+	r = r.Copy().(*render.Sprite)
+	w, h := 16.0, 16.0
+	switch value {
+	case 1:
+		break
+	case 2:
+		// recolor blue
+		r.Filter(recolor.WithStrategy(recolor.ColorShift(color.RGBA{100, 100, 255, 10})))
+	case 3:
+		// recolor purple
+		r.Filter(recolor.WithStrategy(recolor.ColorShift(color.RGBA{255, 100, 255, 10})))
+	case 4:
+		// recolor red
+		r.Filter(recolor.WithStrategy(recolor.ColorShift(color.RGBA{255, 100, 100, 10})))
+	case 5:
+		// size up
+		r.Modify(mod.Scale(2, 2))
+		w *= 2
+		h *= 2
+	}
 	// Todo: calculate image based on value
-	ch.Reactive = entities.NewReactive(0, 0, 16, 16,
+	ch.Reactive = entities.NewReactive(0, 0, w, h,
 		r, nil, ch.Init())
 
 	ch.RSpace.UpdateLabel(labels.Chest)
