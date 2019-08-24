3e23facef07ff072430007d88dd4dcba3c78f199
Futility <cyreal123@gmail.com>
2019-08-24 14:59:39 -0400

    Pulled wood from agent for chest destruction Updates to endgame scene in general

--- internal/end/endGameScene.go
+++ internal/end/endGameScene.go
@@ -5,10 +5,18 @@ import (
 	"image/color"
 	"path/filepath"
 	"strconv"
+	"strings"
+	"time"
 
+	"github.com/200sc/go-dist/floatrange"
+	"github.com/200sc/go-dist/intrange"
 	"github.com/oakmound/oak/collision"
+	"github.com/oakmound/oak/dlog"
 	"github.com/oakmound/oak/event"
 	"github.com/oakmound/oak/mouse"
+	"github.com/oakmound/oak/physics"
+	"github.com/oakmound/oak/render/particle"
+	"github.com/oakmound/oak/shape"
 
 	"github.com/oakmound/oak"
 	"github.com/oakmound/oak/entities/x/btn"
@@ -23,8 +31,30 @@ import (
 	"github.com/oakmound/weekly87/internal/run"
 )
 
+func Init() {
+	_, err := render.LoadSprite("", filepath.Join("raw", "wood_junk.png"))
+	dlog.Error("OH MY " + err.Error())
+}
+
+var (
+	graveX = 90.0
+	graveY = 300.0
+)
+
 var stayInEndScene bool
 var endSceneNextScene string
+
+type intStringer struct {
+	i *int
+}
+
+func (is intStringer) String() string {
+	return strconv.Itoa(*is.i)
+}
+
+var (
+	presentationX, presentationY, startY, pitX, pitY float64
+)
 
 // Scene is a scene in the same package as run to allow for easy variable access.
 //If there is time at the e nd we can look at what vbariables this touches and get them exported or passed onwards so this can have its own package
@@ -39,11 +69,56 @@ var Scene = scene.Scene{
 
 		render.SetDrawStack(layer.Get()...)
 
+		presentationX = float64(oak.ScreenWidth / 2)
+		presentationY = float64(oak.ScreenHeight/2) + 20
+		startY = float64(oak.ScreenHeight/2) + 20
+
+		pitX = float64(oak.ScreenWidth) - 180.0
+		pitY = float64(oak.ScreenHeight) - 100.0
+
+		// partyJson, _ := json.Marshal(runInfo.Party)
+
+		// Update info in the records with info from the most recent run
+		// TODO: debate moving this to the run scene's end function
+		r := records.Load()
+		sc := int64(runInfo.SectionsCleared)
+		r.SectionsCleared += sc
+		if sc > r.FarthestGoneInSections {
+			r.FarthestGoneInSections = sc
+		}
+
+		// Display variables
+		currentDeathToll := r.Deaths
+
+		chestTotal := 0
+		deadChests := 0
+		for _, pl := range runInfo.Party.Players {
+
+			playerChestValue := 0
+			for _, j := range pl.ChestValues {
+				playerChestValue += int(j)
+			}
+			if !pl.Alive {
+				r.Deaths++
+				deadChests += playerChestValue
+			} else {
+				chestTotal += playerChestValue
+			}
+
+		}
+
+		// For the next run
+		r.BaseSeed = int64(runInfo.SectionsCleared) + 1
+		r.Store()
+
 		fnt := render.DefFontGenerator.Copy()
 
 		fnt.Color = render.FontColor("Blue")
 		fnt.Size = 14
 		blueFnt := fnt.Generate()
+		fnt.Color = render.FontColor("Black")
+		fnt.Size = 28
+		graves := fnt.Generate()
 
 		// nextscene = "inn"
 		render.SetDrawStack(layer.Get()...)
@@ -71,16 +146,6 @@ var Scene = scene.Scene{
 
 		textY := 120.0
 
-		fmt.Println("Cleared out", runInfo.SectionsCleared)
-		chestTotal := 0
-		for _, pl := range runInfo.Party.Players {
-			playerChestValue := 0
-			for _, j := range pl.ChestValues {
-				playerChestValue += int(j)
-			}
-			chestTotal += playerChestValue
-		}
-
 		//TODO: 2 layers: first current run
 		// Second layer totals
 		// Each layer has: sections_completed, enemies_defeated, chestvalue
@@ -103,58 +168,67 @@ var Scene = scene.Scene{
 		textX += 200
 		render.Draw(chestValues, 2, 2)
 
-		// partyJson, _ := json.Marshal(runInfo.Party)
+		currentDeathTollp := &currentDeathToll
+		render.Draw(graves.NewText(intStringer{currentDeathTollp}, 180, 300), 1, 10)
 
-		r := records.Load()
-		sc := int64(runInfo.SectionsCleared)
-		r.SectionsCleared += sc
-		if sc > r.FarthestGoneInSections {
-			r.FarthestGoneInSections = sc
+		debugElements := []render.Renderable{}
+		// debug locations
+		debugElements = append(debugElements, render.NewColorBox(5, 5, color.RGBA{200, 200, 10, 255}))
+		debugElements[0].SetPos(presentationX, presentationY)
+
+		debugElements = append(debugElements, render.NewColorBox(5, 5, color.RGBA{200, 200, 200, 255}))
+		debugElements[1].SetPos(graveX, presentationY)
+
+		debugElements = append(debugElements, render.NewColorBox(5, 5, color.RGBA{10, 200, 200, 255}))
+		debugElements[2].SetPos(pitX, pitY)
+		for _, r := range debugElements {
+			render.Draw(r, layer.Debug, 18)
 		}
-		// For the next run
-		// BaseSeed := int64(runInfo.SectionsCleared) + 1
-		r.Store()
+
+		oak.AddCommand("debug", func(args []string) {
+			dlog.Warn("Cheating to toggle debug mode")
+			if debugTree.DrawDisabled {
+				dlog.Warn("Debug turned off")
+				debugTree.DrawDisabled = false
+				for _, r := range debugElements {
+					r.Undraw() // TODO: fix this
+				}
+				return
+			}
+			dlog.Warn("Debug turned on")
+			debugTree.DrawDisabled = true
+			for _, r := range debugElements {
+				render.Draw(r, layer.Debug, 18)
+			}
+
+		})
+
+		dlog.Info("Current Debug Commands are: ", strings.Join(oak.GetDebugKeys(), " , "))
+		oak.AddCommand("help", func(args []string) {
+			dlog.Info("Current Debug Commands are: ", strings.Join(oak.GetDebugKeys(), " , "))
+		})
 
 		// Extra running info
-		presentSpoils(runInfo.Party, 0)
+		presentSpoils(runInfo.Party, currentDeathTollp, 0)
 
 	},
 	Loop: scene.BooleanLoop(&stayInEndScene),
-	// scene.GoTo("inn"),
+
 	End: scene.GoToPtr(&endSceneNextScene),
 }
 
-func presentSpoils(party *players.Party, index int) {
+func presentSpoils(party *players.Party, graveCount *int, index int) {
 	if index > len(party.Players)-1 {
 		return
 	}
 	p := party.Players[index]
 	p.CID = p.Init()
 
-	// location to go to for players to perform their actions
-	presentationX := float64(oak.ScreenWidth / 2)
-	startY := float64(oak.ScreenHeight/2) + 20
-	graveX := 90.0
-	pitX := float64(oak.ScreenWidth) - 180.0
-	pitY := float64(oak.ScreenHeight) - 100.0
-
 	//Player enters stage right
 	p.SetPos(float64(oak.ScreenWidth-64), startY)
 	render.Draw(p.R, layer.Play, 20)
-	fmt.Printf("\nCharacter %d walking through as %s ", index, p.R)
 
-	// debug locations
-	dMid := render.NewColorBox(5, 5, color.RGBA{200, 200, 10, 255})
-	dMid.SetPos(presentationX, p.Y())
-	render.Draw(dMid, layer.Play, 18)
-
-	dGrave := render.NewColorBox(5, 5, color.RGBA{200, 200, 200, 255})
-	dGrave.SetPos(graveX, p.Y())
-	render.Draw(dGrave, layer.Play, 18)
-
-	dPit := render.NewColorBox(5, 5, color.RGBA{10, 200, 200, 255})
-	dPit.SetPos(pitX, pitY)
-	render.Draw(dPit, layer.Play, 18)
+	fmt.Printf("\nCharacter %d walking through and is Alive:%t ", index, p.Alive)
 
 	p.CheckedBind(func(ply *players.Player, _ interface{}) int {
 
@@ -180,9 +254,14 @@ func presentSpoils(party *players.Party, index int) {
 		if ply.R.X() < presentationX {
 
 			if p.Alive {
-				p.CheckedBind(func(ply *players.Player, _ interface{}) int {
 
+				t := time.Now()
+				p.CheckedBind(func(ply *players.Player, _ interface{}) int {
+					if time.Now().After(t) {
+					}
 					//toss
+
+					explodeChest(p.R.X(), p.R.Y())
 
 					ply.R.ShiftY(2)
 					if ply.R.Y() > float64(oak.ScreenHeight) {
@@ -192,25 +271,89 @@ func presentSpoils(party *players.Party, index int) {
 					return 0
 				}, "EnterFrame")
 			} else {
-				p.CheckedBind(func(ply *players.Player, _ interface{}) int {
+				*graveCount++
+				fmt.Printf("Graves is now %d\n ", *graveCount)
+				deadMovement(p)
 
-					// ply.R.Undraw()
-
-					ply.R.ShiftX(-2)
-					if ply.R.X() < graveX {
-
-						return event.UnbindSingle
-					}
-
-					return 0
-				}, "EnterFrame")
 			}
 
-			presentSpoils(party, index+1)
+			presentSpoils(party, graveCount, index+1)
 			return event.UnbindSingle
 
 		}
 		return 0
 	}, "EnterFrame")
 
+}
+
+// var deathParticles particle.Generator
+
+func deadMovement(p *players.Player) {
+	p.CheckedBind(func(ply *players.Player, _ interface{}) int {
+
+		// ply.R.Undraw()
+
+		ply.R.ShiftX(-2)
+		if ply.R.X() < graveX {
+			deathSprites(ply.R.X(), ply.R.Y())
+			ply.R.Undraw()
+			return event.UnbindSingle
+		}
+
+		return 0
+	}, "EnterFrame")
+
+}
+
+func deathSprites(x, y float64) {
+	ptGenLife := intrange.NewLinear(40, 60)
+	ptColor := color.RGBA{200, 200, 200, 255}
+	ptColorRand := color.RGBA{0, 0, 0, 0}
+	newPf := floatrange.NewLinear(5, 10)
+	ptLife := floatrange.NewLinear(100, 200)
+	angle := floatrange.NewLinear(0, 360)
+	speed := floatrange.NewLinear(1, 4)
+	size := intrange.Constant(3)
+	layerFn := func(v physics.Vector) int {
+		return layer.Effect
+	}
+	particle.NewColorGenerator(
+		particle.Pos(x, y),
+		particle.Duration(ptGenLife),
+		particle.LifeSpan(ptLife),
+		particle.Angle(angle),
+		particle.Speed(speed),
+		particle.Layer(layerFn),
+		particle.Shape(shape.Square),
+		particle.Size(size),
+		particle.Color(ptColor, ptColorRand, ptColor, ptColorRand),
+		particle.NewPerFrame(newPf)).Generate(0)
+}
+
+func explodeChest(x, y float64) {
+	sp, err := render.GetSprite(filepath.Join("raw", "wood_junk.png"))
+	if err != nil {
+		dlog.Error(err)
+		return
+	}
+	explodeSprite(x, y, sp)
+}
+func explodeSprite(x, y float64, sprite *render.Sprite) {
+	layerFn := func(v physics.Vector) int {
+		return layer.Effect
+	}
+	ptGenLife := intrange.NewLinear(40, 60)
+	sg := particle.NewSpriteGenerator(
+		particle.NewPerFrame(floatrange.NewSpread(8, 0)),
+		particle.Pos(x, y),
+		particle.LifeSpan(floatrange.NewSpread(20, 5)),
+		particle.Angle(floatrange.NewSpread(0, 360)),
+		particle.Speed(floatrange.NewSpread(2, .5)),
+		particle.Spread(3, 2),
+		particle.Duration(ptGenLife),
+		particle.Layer(layerFn),
+		particle.Sprite(sprite),
+		particle.SpriteRotation(floatrange.Constant(10)),
+	)
+	sg.Generate(layer.Effect)
 }
