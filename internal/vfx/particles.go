25cab46978489791f098a325314efbe7c2161b18
Futility <cyreal123@gmail.com>
2019-09-02 20:26:10 -0400

    Death effect added

--- internal/vfx/particles.go
+++ internal/vfx/particles.go
@@ -23,4 +23,17 @@ var (
 			particle.NewPerFrame(floatrange.NewConstant(40)),
 		)
 	}
+	Death1 = func() particle.Generator {
+		return particle.NewColorGenerator(
+			particle.Color(color.RGBA{100, 158, 200, 255}, color.RGBA{0, 0, 0, 0},
+				color.RGBA{125, 125, 125, 125}, color.RGBA{0, 0, 0, 0}),
+			particle.Shape(shape.Diamond),
+			particle.Size(intrange.NewConstant(10)),
+			particle.EndSize(intrange.NewConstant(5)),
+			particle.Speed(floatrange.NewConstant(2)),
+			particle.LifeSpan(floatrange.NewConstant(2)),
+			particle.Spread(5, 5),
+			particle.NewPerFrame(floatrange.NewConstant(40)),
+		)
+	}
 )
