46f5b01407b8a68b3db39f61b84856f6cdca596d
Futility <cyreal123@gmail.com>
2019-09-07 08:05:28 -0400

    Stop gap for shaking implemented. Need to implement changes in oak shaker to correct for multiple shakers

--- internal/vfx/shakes.go
+++ internal/vfx/shakes.go
@@ -2,20 +2,47 @@
 package vfx
 
 import (
+	"sync"
+	"time"
+
 	"github.com/oakmound/oak"
 	"github.com/oakmound/oak/alg/floatgeom"
 )
 
 var (
-	SmallShaker oak.ScreenShaker
+	SmallShaker Shaker
 )
 
-func Init() {
-	SmallShaker = oak.ScreenShaker{
-		Random: false,
-		Magnitude: floatgeom.Point2{
-			3,
-			3,
+func init() {
+	SmallShaker = Shaker{
+		mu: &sync.Mutex{},
+		shaker: oak.ScreenShaker{
+			Random: false,
+			Magnitude: floatgeom.Point2{
+				3,
+				3,
+			},
 		},
+		shakingEnd: time.Now(),
 	}
+}
+
+// Shaker exposes a simple oak shaker that refuses shakes if already shaking
+// This should be updated later
+type Shaker struct {
+	mu         *sync.Mutex
+	shaker     oak.ScreenShaker
+	shakingEnd time.Time
+}
+
+func (s *Shaker) Shake(shaking time.Duration) {
+	s.mu.Lock()
+	if time.Now().Before(s.shakingEnd) {
+		s.mu.Unlock()
+		return
+	}
+	s.shakingEnd = time.Now().Add(shaking)
+	s.mu.Unlock()
+
+	s.shaker.Shake(shaking)
 }
