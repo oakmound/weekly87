c98f7b624aba4a39c08fc5eeb6e213d8855493ed
Patrick Stephen <patrick.d.stephen@gmail.com>
2019-11-02 12:56:51 -0500

    Adding new menu colors

---
+++ internal/menus/colors.go
@@ -0,0 +1,19 @@
+package menus
+
+import (
+	"image/color"
+)
+
+// Standard menu colors
+var (
+	Red = color.RGBA{151,46,46,255}
+	Green = color.RGBA{92, 133, 62, 255}
+	Blue  = color.RGBA{75, 104, 155, 255}
+	Purple = color.RGBA{101,82,120, 255}
+	Gray = color.RGBA{155,152,145, 255}
+	Gold = color.RGBA{159,127,68, 255}
+	Yellow = Gold
+	Brown = color.RGBA{155,95,64, 255}
+	Orange = Brown
+	LightBlue = color.RGBA{126, 150, 165, 255}
+)
